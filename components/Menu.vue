742aabc47493438bfcf9c04f10e696bc6b6a50d0
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-01-05 17:35:43 -0300

    Incicio do projeto

---
+++ components/Menu.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav>
+    https://www.youtube.com/watch?v=vUcUdpBcmF8
+    <div v-for="item in menu" :key="item.title" class="menu-item">
+      {{ item.title }}
+      <a :href="item.link"></a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      menu: [
+        {
+          title: 'Home',
+          link: '/home',
+        },
+        {
+          title: 'Sobre',
+          link: '/sobre',
+        },
+        {
+          title: 'Contato',
+          link: '/contato',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+nav .menu-item {
+  color: aliceblue;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.3s;
+}
+
+nav .menu-item.active,
+nav .menu-item:hover {
+  background-color: rgb(44, 44, 44);
+  border-bottom-color: rgb(255, 225, 0);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,11 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">website</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <header><Menu /></header>
+    <NuxtLink to="/home">Home</NuxtLink>
+    <NuxtLink to="/sobre">Sobre</NuxtLink>
+    <NuxtLink to="/contato">Contato</NuxtLink>
+    <Nuxt />
+    <footer>Footer</footer>
   </div>
 </template>
 
@@ -32,34 +16,20 @@ export default Vue.extend({})
 </script>
 
 <style>
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
 .container {
-  margin: 0 auto;
+  margin: 0;
+  padding: 0;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  box-sizing: border-box;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+header {
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
 }
 </style>
